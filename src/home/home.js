e6cea58a9998fa9f70498c70563a6812888a396e
unknown <changelaom@gmail.com>
2024-10-31 00:28:17 +0530

    service update in home page

--- src/home/home.js
+++ src/home/home.js
@@ -14,7 +14,15 @@ import hhProject3 from "../images/home/hProject3.jpg";
 import customer1 from "../images/home/customer.jpg";
 
 export function Home() {
- 
+  const projects = [
+    { title: "Decoration Work", img: hPoject1 },
+    { title: "Interior Work", img: hhProject2 },
+    { title: "Project Plan", img: hhProject3 },
+    { title: "Interior design", img: hhProject2 },
+  ];
+
+  const loopedProjects = [...projects]; // Duplicate the projects array
+
   return (
     <div className="home">
       <div className="homeNews" style={{ backgroundImage: `url(${photoBg})` }}>
@@ -52,142 +60,94 @@ export function Home() {
             })}
         </div>
         <div className="homeAboutUs">
-            <div className="hp-subtext">
-              <h1>We Create The Art Of Stylish Living Stylishly</h1>
-              <p>
-                It is a long established fact that a reader will be distracted
-                by the of readable content of a page when lookings at its
-                layouts the points of using that it has a more-or-less normal.
-              </p>
-              <div className="callUs">
-          <div className="phoneNum">
-            <p className="h-icon">
-              <BsTelephone />
+          <div className="hp-subtext">
+            <h1>We Create The Art Of Stylish Living Stylishly</h1>
+            <p>
+              It is a long established fact that a reader will be distracted
+              by the readable content of a page when looking at its layout.
             </p>
-            <a href="tel: +91 9999999999">
-              <p>
-                +91 9999999999
-                <br />
-                <span>Call Us Anytime</span>
-              </p>
-            </a>
-            <br />
-          </div>
+            <div className="callUs">
+              <div className="phoneNum">
+                <p className="h-icon">
+                  <BsTelephone />
+                </p>
+                <a href="tel: +91 9999999999">
+                  <p>
+                    +91 9999999999
+                    <br />
+                    <span>Call Us Anytime</span>
+                  </p>
+                </a>
+              </div>
 
-          <Link to={`/contact`}>
-            <button>
-              Get Free Estimate
-              <BsArrowRight style={{ marginLeft: "2%", color: "#ffffff" }} />
-            </button>
-          </Link>
-        </div>
+              <Link to={`/contact`}>
+                <button>
+                  Get Free Estimate
+                  <BsArrowRight style={{ marginLeft: "2%", color: "#ffffff" }} />
+                </button>
+              </Link>
             </div>
-            <div className="hp-img">
-              <img src={home1} alt="concept"></img>
-            </div>
+          </div>
+          <div className="hp-img">
+            <img src={home1} alt="concept" />
+          </div>
         </div>
-   
+
         <div className="people-thoughts">
           <h1>What People Think <br /> About Us</h1>
           <div className="people">
-            {thoughts.people.map((req, ind) => {
-              return (
-                <div className="person" key={ind}>
-                  <div className="person-title">
-                    <div>
-                      <img src={customer1} alt="customer"></img>
-                    </div>
-                    <p>
-                      {req.fullname}
-                      <br />
-                      <span>{req.country}</span>
-                    </p>
+            {thoughts.people.map((req, ind) => (
+              <div className="person" key={ind}>
+                <div className="person-title">
+                  <div>
+                    <img src={customer1} alt="customer" />
                   </div>
-                  <p className="thought">{req.thoughts}</p>
+                  <p>
+                    {req.fullname}
+                    <br />
+                    <span>{req.country}</span>
+                  </p>
                 </div>
-              );
-            })}
+                <p className="thought">{req.thoughts}</p>
+              </div>
+            ))}
           </div>
         </div>
-        <div className="home-brands">
-          
-        </div>
+
         <div className="homeProjects">
-          <h1> Our Services</h1>
+          <h1>Our Services</h1>
           <p>
-            It is a long established fact that a reader will be distracted by
-            the of readable content of a page lookings at its layouts.
+            It is a long-established fact that a reader will be distracted by the
+            readable content of a page while looking at its layout.
           </p>
-          <div className="hp-list">
-            <div className="hp-project">
-            <Link to={`/project-details`}>
-              <div className="hp-pro-img">
-                <img src={hPoject1} alt="project"></img>
-              </div>
-             
-              <div className="hp-pro-detail">
-                <div className="hp-pro-info">
-                  <p className="hp-prj-title">Decoration Work
-                  </p>
-                  <p className="hp-prj-path">Decor / Architecture</p>
-                </div>
-                <div className="hp-pro-btn">
-                <Link to={`/project-details`}>
-                    <button>
-                      <IoIosArrowForward />
-                    </button>
-                    </Link>
-                </div>
-              </div>
-              </Link>
-            </div>
-
-            <div className="hp-project">
-            <Link to={`/project-details`}>
-              <div className="hp-pro-img">
-                <img src={hhProject2} alt="project"></img>
-              </div>
-              <div className="hp-pro-detail">
-                <div className="hp-pro-info">
-                  <p className="hp-prj-title">Interior Work
-                  </p>
-                  <p className="hp-prj-path">Decor / Architecture</p>
-                </div>
-                <div className="hp-pro-btn">
+          <div className="hp-list scroll-container">
+            <div className="hp-list-inner">
+              {[...loopedProjects, ...loopedProjects].map((project, index) => (
+                <div key={index} className="hp-project flex-shrink-0">
                   <Link to={`/project-details`}>
-                    <button>
-                      <IoIosArrowForward />
-                    </button>
+                    <div className="hp-pro-img">
+                      <img src={project.img} alt="project" />
+                    </div>
+                    <div className="hp-pro-detail">
+                      <div className="hp-pro-info">
+                        <p className="hp-prj-title">{project.title}</p>
+                        <p className="hp-prj-path">Decor / Architecture</p>
+                      </div>
+                      <div className="hp-pro-btn">
+                        <button>
+                          <IoIosArrowForward />
+                        </button>
+                      </div>
+                    </div>
                   </Link>
                 </div>
-              </div>
-              </Link>
+              ))}
             </div>
-
-            <div className="hp-project">
-            <Link to={`/project-details`}>
-              <div className="hp-pro-img">
-                <img src={hhProject3} alt="project"></img>
-              </div>
-              <div className="hp-pro-detail">
-                <div className="hp-pro-info">
-                  <p className="hp-prj-title">Project Plan</p>
-                  <p className="hp-prj-path">Decor / Architecture</p>
-                </div>
-                <div className="hp-pro-btn">
-                  <Link to={`/project-details`}>
-                    <button>
-                      <IoIosArrowForward />
-                    </button>
-                  </Link>
-                </div>
-              </div>
-              </Link>
-            </div>
-
           </div>
         </div>
+
       </div>
+
       <div className="h-interno">
         <h1>Discuss Project With Us</h1>
         <p>It is a long established fact will be distracted.</p>
